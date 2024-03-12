67b18085972189e928e8de564049727db30d661c
AlenaKharytonchyk <alena_kharytonchyk@epam.com>
2024-03-12 14:04:03 +0300

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,54 @@
 import fs from "fs";
 import path from "path";
+import xml2json from "xml2json";
+import {cuurentColorVariables} from "./currentColorVariables";
+
+export interface Config {
+  scripts: Scripts;
+}
+
+export interface Scripts {
+  bundles: Bundles;
+}
+
+export interface Bundles {
+  bundle: Bundle[];
+}
+
+export interface Bundle {
+  name:  string;
+  debug: Debug;
+  prod:  Debug;
+}
+
+export interface Debug {
+  file: FileElement[] | FileElement;
+}
+
+export interface FileElement {
+  src: string;
+}
+const cssVariables = Object.keys(cuurentColorVariables).join("|");
+const cssVariableRegex = new RegExp(`var\\((${cssVariables})`, "gi");
+const regularColorsRegex = /:\s?(black|white|red|green|blue|yellow|orange|pink|purple|brown|gray|grey)/i;
 
 const filePath = process.argv[2];
+const scriptConfig = process.argv[3];
 const outputJson = path.join(__dirname, "output.json");
+
+const xmlContent = fs.readFileSync(scriptConfig, "utf-8");
+const jsonContent = xml2json.toJson(xmlContent);
+const parsedJson: Config = JSON.parse(jsonContent);
+
+const fileList = parsedJson.scripts.bundles.bundle.flatMap((bundle) => {
+  const debugFiles = bundle.debug.file;
+  if (Array.isArray(debugFiles)) {
+    return debugFiles.map((file) => ({path: file.src.replace(/\//gi, '\\').replace('\\litmos\\', ''), bundleName: bundle.name}));
+  }
+  return [{path: debugFiles.src.replace('/', '\\').replace('\\litmos\\', ''), bundleName: bundle.name}];
+})
+
+fs.writeFileSync(outputJson, JSON.stringify(fileList, null, 2));
 
 if (!filePath) {
   console.error("No path provided");
@@ -18,7 +64,7 @@ const findCSSFiles = (filePath: string): string[] => {
       result = result.concat(findCSSFiles(fullPath));
     }
     if (file.endsWith(".css")) {
-      result.push(fullPath);
+      result.push(fullPath.toLocaleLowerCase());
     }
   });
   return result;
@@ -26,6 +72,7 @@ const findCSSFiles = (filePath: string): string[] => {
 
 interface FileScan {
   filePath: string;
+  bundleName: string;
   foundLines: {
     lineNumber: number;
     lineContent: string;
@@ -43,13 +90,23 @@ const scanCSSFiles = (files: string[]) => {
       const content = fs.readFileSync(file, "utf-8");
       const fileScanResult: FileScan = {
         filePath: file,
+        bundleName: fileList.find((f) => file.includes(f.path))?.bundleName || "",
         foundLines: [],
       };
 
       const lines = content.split("\n");
 
       lines.forEach((line, index) => {
-        if (line.match(/#[0-9a-fA-F]{6}/g) || line.match(/#[0-9a-fA-F]{3}/g)) {
+        if (
+          line.match(/#[0-9a-fA-F]{6}[;,\s]/g)
+          || line.match(/#[0-9a-fA-F]{3}[;,\s]/g) 
+          || line.match(/rgba?\([0-9]{1,3},[0-9]{1,3},[0-9]{1,3}/g) 
+          || line.match(/hsla?\([0-9]{1,3},[0-9]{1,3},[0-9]{1,3}/g) 
+          || line.match(/rgb\([0-9]{1,3},[0-9]{1,3},[0-9]{1,3}/g) 
+          || line.match(/hsl\([0-9]{1,3},[0-9]{1,3},[0-9]{1,3}/g)
+          || line.match(cssVariableRegex)
+          || line.match(regularColorsRegex)
+          ) {
           fileScanResult.foundLines.push({
             lineNumber: index + 1,
             lineContent: line,
@@ -62,12 +119,14 @@ const scanCSSFiles = (files: string[]) => {
       return acc.concat(fileScanResult);
     }, [] as FileScan[]);
 
-  const csvOutput = output.flatMap((file) => file.foundLines.map((line) => `${file.filePath}, ${line.lineNumber}, ${line.lineContent.trim()}`));
-  csvOutput.unshift("File Path, Line Number, Line Content");
+  const entries = Object.entries(cuurentColorVariables)
+
+  const csvOutput = output.flatMap((file) => file.foundLines.map((line) => `${file.filePath}| ${file.bundleName}| ${line.lineNumber}| ${line.lineContent.trim()}| ${entries.filter(([, value]) => line.lineContent.toLowerCase().includes(value.toLowerCase())).map(([key]) => key).join(",")}`));
+  csvOutput.unshift("File Path| Bundle Name| Line Number| Line Content| CSS Variable Name");
 
 
   fs.writeFileSync(path.join(__dirname, "output.csv"), csvOutput.join("\n"));
-  fs.writeFileSync(outputJson, JSON.stringify(output, null, 2));
+  // fs.writeFileSync(outputJson, JSON.stringify(output, null, 2));
 };
 
 const cssFiles = findCSSFiles(filePath);
