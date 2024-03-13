3b33982aa92bdce29ba328e5ac3de8229cdc7f22
Artsiom Kharytonchyk <artsiom.kharytonchyk@coxautoinc.com>
2024-03-13 15:49:17 +0300

    fix: typo

--- src/colorMagic.ts
+++ src/colorMagic.ts
@@ -1,11 +1,18 @@
 import { Colord } from "colord";
 
-
 export const sortColors = (colorCount: Record<string, number>) => {
-
   const colors = Object.keys(colorCount)
-    .map((color) => ({ color, count: colorCount[color], hsl: new Colord(color).toHsv()}))
-    .sort((a, b) => a.hsl.h - b.hsl.h); 
+    .filter((color) => !color.includes("var"))
+    .map((color) => ({
+      color,
+      count: colorCount[color],
+      hsl: new Colord(color).toHsl(),
+    }))
+    .sort((a, b) => {
+      if (a.hsl.h !== b.hsl.h) return a.hsl.h - b.hsl.h;
+      if (a.hsl.s !== b.hsl.s) return a.hsl.s - b.hsl.s;
+      return a.hsl.l - b.hsl.l;
+    });
 
   return colors;
 };
