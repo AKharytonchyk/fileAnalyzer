8ffc65f0c7187b254b52ad34f0bc8265d1dedf04
Artsiom Kharytonchyk <artsiom.kharytonchyk@coxautoinc.com>
2024-03-15 14:37:34 +0300

    feat: merged

---
+++ src/colors/colordColorComparison.ts
@@ -0,0 +1,50 @@
+import { Colord, HslaColor, LabaColor, extend } from "colord";
+import labPlugin from "colord/plugins/lab";
+import namesPlugin from "colord/plugins/names";
+
+extend([labPlugin, namesPlugin]);
+
+interface ColorInfo {
+  variable: string;
+  hsl: HslaColor;
+  lab: LabaColor;
+  hex: string;
+  name: string;
+  baseColor?: string;
+  difference?: number;
+  raw: Colord;
+}
+
+const parseColors = (colors: string[]): ColorInfo[] => {
+    return colors
+      .filter((color) => !color.includes('var'))
+      .map((color) => ({color, raw: new Colord(color)}))
+      .map(({color, raw}) => ({
+        variable: color,
+        hsl: raw.toHsl(),
+        lab: raw.toLab(),
+        hex: raw.toHex(),
+        name: raw.toName({ closest: true }) || 'unknown',
+        raw
+    }))
+}
+
+export const treeShakeColors = (colors: string[], colorPallet: string[], deltaThreshold: number = Infinity) => {
+    const parsedColors = parseColors(colors);
+    const parsedBaseColors = parseColors(colorPallet);
+
+    parsedColors.map((color) => {
+      let minDifference = deltaThreshold;
+      let closestBaseColor: ColorInfo | undefined;
+
+      parsedBaseColors.forEach((baseColor) => {
+        const diff = baseColor.raw.delta(color.raw);
+        if (diff < minDifference) {
+          minDifference = diff;
+          closestBaseColor = baseColor;
+        }
+      });
+
+      return {...color, baseColor: closestBaseColor?.variable, difference: minDifference}
+    })
+}
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,9 @@ import fs from "fs";
 import path from "path";
 import { currentColorVariables } from "./currentColorVariables";
 import { sortColors } from "./colorMagic";
-import { calculateDifferencesForAllColors } from "./colors/pyCalculator";
 import { getFilesFromBundle } from "./crawler/getFilesFromBundle";
-import { getCssFilesFromJs } from "./crawler/jsfileParser";
 import { getCssFilesFromHtml } from "./crawler/htmlFileParser";
+import { getCssFilesFromJs } from "./crawler/jsfileParser";
 export interface Config {
   scripts: Scripts;
 }
